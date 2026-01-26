37d8f306234e1d38223904c06ea43a8ba12849b1
miranautaa <srcopito657@gmail.com>
2026-01-26 20:29:58 -0300

    idk?

--- SlendyTubbies Plus/Assets/Tinkyvision.js
+++ SlendyTubbies Plus/Assets/Tinkyvision.js
@@ -1,18 +1,33 @@
 #pragma strict
 
-private var fogApplied : boolean = false;
+public var thePlayer: Transform;
+public var playerIndic: Transform;
+private var fogApplied: boolean = false;
 
+function Start() {
+    if (thePlayer == null) {
+        thePlayer = GameObject.FindWithTag("Player").transform;
+    }
+}
 function Update()
 {
     // Tinky invisible on
     if (EnemyScriptPlayer.tinkyInvisible && !fogApplied)
     {
+        Debug.Log("FogDisabled");
         RenderSettings.fog = false;
         fogApplied = true;
+        Network.Instantiate(playerIndic, Vector3(thePlayer.position.x, thePlayer.position.y + 1.07, thePlayer.position.z), thePlayer.rotation, 1);
     }
     else if (!EnemyScriptPlayer.tinkyInvisible && fogApplied)
     {
+        Debug.Log("FogEnabled");
         RenderSettings.fog = true;
         fogApplied = false;
+        var numbIndic = GameObject.FindGameObjectsWithTag("PlayerIndicator");
+        for (var obj in numbIndic) 
+        {
+            Destroy(obj);
+        }
     }
 }
